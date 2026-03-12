83f0f3b5184c14527cef07d0071be8a10b16a7d3
Serafimova <kassarety1980@gmail.com>
2026-03-12 16:09:11 +0300

    CLOSE #11
    Related Work Items: #7

---
+++ prototip/CaptchaForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace prototip
+{
+    partial class CaptchaForm
+    {
+
+        private System.Windows.Forms.PictureBox pbCaptcha;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox txtCaptcha;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CaptchaForm));
+            this.pbCaptcha = new System.Windows.Forms.PictureBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtCaptcha = new System.Windows.Forms.TextBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaptcha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCaptcha
+            // 
+            this.pbCaptcha.BackColor = System.Drawing.Color.White;
+            this.pbCaptcha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCaptcha.Location = new System.Drawing.Point(50, 50);
+            this.pbCaptcha.Name = "pbCaptcha";
+            this.pbCaptcha.Size = new System.Drawing.Size(350, 120);
+            this.pbCaptcha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbCaptcha.TabIndex = 0;
+            this.pbCaptcha.TabStop = false;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRefresh.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRefresh.Location = new System.Drawing.Point(150, 180);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(150, 35);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "🔄 Новый код";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            // 
+            // txtCaptcha
+            // 
+            this.txtCaptcha.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtCaptcha.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtCaptcha.Location = new System.Drawing.Point(120, 230);
+            this.txtCaptcha.MaxLength = 4;
+            this.txtCaptcha.Name = "txtCaptcha";
+            this.txtCaptcha.Size = new System.Drawing.Size(200, 34);
+            this.txtCaptcha.TabIndex = 2;
+            this.txtCaptcha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTimer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTimer.Location = new System.Drawing.Point(100, 280);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(0, 21);
+            this.lblTimer.TabIndex = 3;
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTimer.Visible = false;
+            // 
+            // btnOk
+            // 
+            this.btnOk.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOk.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOk.Location = new System.Drawing.Point(80, 320);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(130, 40);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "✓ Проверить";
+            this.btnOk.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancel.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.Location = new System.Drawing.Point(240, 320);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 40);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "✗ Отмена";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(100, 280);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 19);
+            this.lblError.TabIndex = 4;
+            this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblError.Visible = false;
+            // 
+            // CaptchaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(449, 386);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.txtCaptcha);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.pbCaptcha);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CaptchaForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Проверка CAPTCHA";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaptcha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ prototip/CaptchaForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prototip
+{
+    public partial class CaptchaForm : Form
+    {
+        private string captchaCode;
+        private Bitmap captchaImage;
+        private Timer blockTimer;
+        private int failedAttempts = 0;
+        private bool isBlocked = false;
+        private int blockTimeSeconds = 10;
+
+       
+
+        public string EnteredCode { get; private set; }
+        public CaptchaForm()
+        {
+            InitializeComponent();
+            GenerateNewCaptcha();
+            this.btnRefresh.Click += BtnRefresh_Click;
+            this.txtCaptcha.TextChanged += (s, e) => EnteredCode = txtCaptcha.Text;
+        }
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            if (!isBlocked)
+            {
+                GenerateNewCaptcha();
+                txtCaptcha.Clear();
+            }
+        }
+
+        private void GenerateNewCaptcha()
+        {
+            if (isBlocked) return;
+
+            // Простой код из 4 символов
+            Random rnd = new Random();
+            string chars = "ABCDEFGHJKLMNPQRSTUVWXYZ0123456789";
+            captchaCode = "";
+            for (int i = 0; i < 4; i++)
+            {
+                captchaCode += chars[rnd.Next(chars.Length)];
+            }
+
+            // Простое изображение
+            captchaImage = new Bitmap(350, 120);
+            using (Graphics g = Graphics.FromImage(captchaImage))
+            {
+                g.Clear(Color.White);
+
+                // Рисуем символы в линию
+                for (int i = 0; i < captchaCode.Length; i++)
+                {
+                    using (Font font = new Font("Arial", 30, FontStyle.Bold))
+                    using (Brush brush = new SolidBrush(Color.Black))
+                    {
+                        g.DrawString(captchaCode[i].ToString(), font, brush, 50 + i * 70, 40);
+                    }
+                }
+
+                // Немного линий для шума
+                for (int i = 0; i < 5; i++)
+                {
+                    using (Pen pen = new Pen(Color.Gray, 1))
+                    {
+                        g.DrawLine(pen, rnd.Next(350), rnd.Next(120), rnd.Next(350), rnd.Next(120));
+                    }
+                }
+            }
+
+            if (pbCaptcha.Image != null)
+                pbCaptcha.Image.Dispose();
+            pbCaptcha.Image = captchaImage;
+        }
+
+        public bool ValidateCaptcha(string input)
+        {
+            if (isBlocked)
+            {
+                MessageBox.Show($"Подождите {blockTimeSeconds} секунд!", "Блокировка");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(input) || input.Length != 4)
+            {
+                lblError.Text = "Введите 4 символа!";
+                lblError.Visible = true;
+                return false;
+            }
+
+            if (input.ToUpper() == captchaCode.ToUpper())
+            {
+                failedAttempts = 0;
+                lblError.Visible = false;
+                return true;
+            }
+            else
+            {
+                failedAttempts++;
+                lblError.Text = "Неверный код!";
+                lblError.Visible = true;
+
+                if (failedAttempts >= 1)
+                {
+                    isBlocked = true;
+                    int timeLeft = blockTimeSeconds;
+
+                    btnRefresh.Enabled = false;
+                    txtCaptcha.Enabled = false;
+                    btnOk.Enabled = false;
+
+                    blockTimer = new Timer();
+                    blockTimer.Interval = 1000;
+                    blockTimer.Tick += (s, e) =>
+                    {
+                        timeLeft--;
+                        lblTimer.Text = $"Блокировка: {timeLeft} сек";
+
+                        if (timeLeft <= 0)
+                        {
+                            blockTimer.Stop();
+                            isBlocked = false;
+                            btnRefresh.Enabled = true;
+                            txtCaptcha.Enabled = true;
+                            btnOk.Enabled = true;
+                            lblTimer.Text = "";
+                            GenerateNewCaptcha();
+                        }
+                    };
+                    blockTimer.Start();
+                }
+
+                GenerateNewCaptcha();
+                txtCaptcha.Clear();
+                return false;
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            blockTimer?.Stop();
+            pbCaptcha.Image?.Dispose();
+            base.OnFormClosing(e);
+        }
+ 
+
+}
+}
